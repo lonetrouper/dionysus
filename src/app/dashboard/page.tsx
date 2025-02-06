2b47079a48b09a53bb2ce04dac94a361184dc62b
Pratush Bose <pratush.bose@whatfix.com>
2025-02-06 09:19:11 +0530

    Initial setup of clerk redirection and storing user details in db

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { useUser } from "@clerk/nextjs";
+
+const Dashboard = () => {
+  const { user } = useUser();
+  return ( <div>{user?.firstName}</div> );
+}
+ 
+export default Dashboard;
