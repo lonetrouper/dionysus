b78a578c2284ab520bb95a08967b1918c51c9dd8
Pratush Bose <pratush.bose@whatfix.com>
2025-02-07 12:15:04 +0530

    Adding some sidebar and creating a layout for the application

---
+++ src/app/(protected)/AppSideBar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react";
+import Link from "next/link";
+import { it } from "node:test";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "~/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Dasboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Q&A",
+    url: "/qa",
+    icon: Bot,
+  },
+  {
+    title: "Meetings",
+    url: "/meetings",
+    icon: Presentation,
+  },
+  {
+    title: "Billing",
+    url: "/billing",
+    icon: CreditCard,
+  },
+];
+const AppSideBar = () => {
+  return (
+    <Sidebar collapsible="icon" variant="floating">
+      <SidebarHeader>Logo</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            {items.map((item) => {
+              return (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link
+                      href={item.url}
+                      className="{cn({'!bg-primary !text-white': pathname === item.url}, 'list-none')}"
+                    >
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              );
+            })}
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default AppSideBar;
