8cb7df72de452040230e2e1ce8b76bd4e2e4d53a
Pratush Bose <bosepratush@gmail.com>
2025-02-10 10:56:35 +0530

    Completing the UI for create page

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,5 +1,8 @@
+"use client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 
 type FormInput = {
   repoUrl: string;
@@ -8,10 +11,46 @@ type FormInput = {
 };
 
 const CreatePage = () => {
-//   const { register, handleSubmit, reset } = useForm<FormInput>();
+  const { register, handleSubmit, reset } = useForm<FormInput>();
+  const onSubmit = async (data: FormInput) => {
+    return true;
+  };
   return (
     <div className="flex h-full items-center justify-center gap-12">
-      <img src="/github.png" className="h-56 w-auto" />
+      <img src="/repository.png" className="h-56 w-auto" />
+      <div>
+        <div>
+          <h1 className="text-2xl font-semibold">
+            Link your Github Repository
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter the URL of your repository to link it to ByteBlaze
+          </p>
+        </div>
+        <div className="h-4"></div>
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              {...register("projectName", { required: true })}
+              placeholder="ProjectName"
+              required
+            />
+            <div className="h-2"></div>
+            <Input
+              {...register("repoUrl", { required: true })}
+              placeholder="Github URL"
+              type="url"
+              required
+            />
+            <div className="h-2"></div>
+            <Input
+              {...register("githubToken")}
+              placeholder="ProjectName"
+            />
+            <Button type="submit">Create Project</Button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
