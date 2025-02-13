a61abcf947c3ec6267fe2b32f376df43d8a06135
Pratush Bose <bosepratush@gmail.com>
2025-02-13 14:04:00 +0530

    boilerplate for gemini api

---
+++ src/lib/gemini.ts
@@ -0,0 +1,10 @@
+import { GoogleGenerativeAI} from '@google/generative-ai'
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
+const model = genAI.getGenerativeModel({
+    model: 'gemini-1.5-flash'
+}) 
+
+export const summariseCommit = async (diff: string) => {
+    
+}
