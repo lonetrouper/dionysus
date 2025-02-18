ab298f542749ec146e495f7aaa51b62694fb30ba
Pratush Bose <bosepratush@gmail.com>
2025-02-18 09:16:25 +0530

    started work on github loaded using langchain

---
+++ src/lib/github-loader.ts
@@ -0,0 +1,51 @@
+import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import { Document } from "@langchain/core/documents";
+import { warn } from "console";
+import { generateEmbedding, summariseCode } from "./gemini";
+export const loadGithubRepo = async (
+  githubUrl: string,
+  githubToken?: string,
+) => {
+  const loader = new GithubRepoLoader(githubUrl, {
+    accessToken: githubToken || "",
+    branch: "main",
+    ignoreFiles: [
+      "package-lock.json",
+      "yarn.lock",
+      "pnpm-lock.yaml",
+      "bun.lockb",
+    ],
+    recursive: true,
+    unknown: "warn",
+    maxConcurrency: 5,
+  });
+  const docs = await loader.load();
+  return docs;
+};
+
+export const indexGithubRepo = async (
+  projectId: string,
+  githubUrl: string,
+  githubToken?: string,
+) => {
+  const docs = await loadGithubRepo(githubUrl, githubToken);
+  const allEmbeddings = await generateEmbeddings(docs);
+  await Promise.allSettled(allEmbeddings.map((embedding, index) => {
+    console.log(`processing ${index}`)
+  }))
+};
+
+const generateEmbeddings = async (docs: Document[]) => {
+  return await Promise.all(
+    docs.map(async (doc) => {
+      const summary = await summariseCode(doc);
+      const embedding = await generateEmbedding(summary);
+      return {
+        summary,
+        embedding,
+        sourceCode: JSON.parse(JSON.stringify(doc.pageContent)),
+        fileName: doc.metadata.source,
+      };
+    }),
+  );
+};
