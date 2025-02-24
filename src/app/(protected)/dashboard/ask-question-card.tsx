7d040b98be4f4bff2f3d0c577d46d0b6dffca60a
Pratush Bose <bosepratush@gmail.com>
2025-02-24 09:45:08 +0530

    added the ask-question card

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -1,25 +1,54 @@
 "use client";
+import MDEditor from "@uiw/react-md-editor";
 import Image from "next/image";
 import React from "react";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "~/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
 import { Textarea } from "~/components/ui/textarea";
 import useProject from "~/hooks/use-project";
+import { askQuestion } from "./actions";
+import { readStreamableValue } from "ai/rsc";
+import CodeReferences from "./code-references";
 
 const AskQuestionCard = () => {
   const { project } = useProject();
   const [open, setOpen] = React.useState(false);
   const [question, setQuestion] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [filesReferences, setFilesReferences] = React.useState<
+    { fileName: string; sourceCode: string; summary: string }[]
+  >([]);
+  const [answer, setAnswer] = React.useState("");
+
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setAnswer("");
+    setFilesReferences([]);
     e.preventDefault();
-    // window.alert(question);
+    if (!project?.id) return;
+
+    setLoading(true);
+
+    const { output, filesReferenced } = await askQuestion(question, project.id);
     setOpen(true);
+    setFilesReferences(filesReferenced);
+
+    for await (const delta of readStreamableValue(output)) {
+      if (delta) {
+        setAnswer((ans) => ans + delta);
+      }
+    }
+    setLoading(false);
   };
   return (
     <>
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent>
+        <DialogContent className="sm:max-w-[80vw]" data-color-mode="light">
           <DialogHeader>
             <DialogTitle>
               <Image
@@ -30,6 +59,15 @@ const AskQuestionCard = () => {
               />
             </DialogTitle>
           </DialogHeader>
+          <MDEditor.Markdown
+            source={answer}
+            className="!h-full max-h-[40vh] max-w-[70vw] overflow-scroll"
+          />
+          <div className="h-4"></div>
+          <CodeReferences filesReferences={filesReferences}/>
+          <Button type="button" onClick={() => setOpen(false)}>
+            Close
+          </Button>
         </DialogContent>
       </Dialog>
       <Card className="relative col-span-3">
@@ -44,7 +82,9 @@ const AskQuestionCard = () => {
               onChange={(e) => setQuestion(e.target.value)}
             />
             <div className="h-4"></div>
-            <Button type="submit">Ask ByteBlaze!</Button>
+            <Button type="submit" disabled={loading}>
+              Ask ByteBlaze!
+            </Button>
           </form>
         </CardContent>
       </Card>
