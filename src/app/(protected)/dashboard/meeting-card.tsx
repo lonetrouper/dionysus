631473547bb0ada680366e161882d31a7d2ff9f4
Pratush Bose <bosepratush@gmail.com>
2025-02-26 21:19:24 +0530

    added meeting-card for dashboard

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -1,12 +1,24 @@
 "use client";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { Presentation, Upload } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
+import { Button } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
 import { uploadFile } from "~/lib/firebase";
+import { api } from "~/trpc/react";
+import useProject from "~/hooks/use-project";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const MeetingCard = () => {
+  const { project } = useProject();
   const [isUploading, setIsUploading] = React.useState(false);
   const [progress, setProgress] = React.useState(0);
+  const router = useRouter();
+
+  const uploadMeeting = api.project.uploadMeeting.useMutation();
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
       "audio/*": [".mp3", ".wav", ".m4a"],
@@ -14,19 +26,80 @@ const MeetingCard = () => {
     multiple: false,
     maxSize: 50_000_000,
     onDrop: async (acceptedFiles) => {
+      if (!project) {
+        return;
+      }
       setIsUploading(true);
       console.log(acceptedFiles);
+      console.log("isuploading", isUploading);
       const file = acceptedFiles[0];
-      uploadFile(file as File, setProgress);
+      if (!file) {
+        return;
+      }
+      const downloadURL = (await uploadFile(
+        file as File,
+        setProgress,
+      )) as string;
+      uploadMeeting.mutate(
+        {
+          projectId: project.id,
+          meetingUrl: downloadURL,
+          name: file.name,
+        },
+        {
+          onSuccess: () => {
+            toast.success("Meeting uploaded successfully");
+            router.push("/meetings");
+          },
+          onError: (error) => {
+            toast.error(error.message);
+          },
+        },
+      );
       setIsUploading(false);
     },
   });
   return (
     <Card
-      className="col-span-2 flex flex-col items-center justify-center"
+      className="col-span-2 flex flex-col items-center justify-center p-10"
       {...getRootProps()}
     >
-      <div></div>
+      {!isUploading && (
+        <>
+          <Presentation className="h-10 w-10 animate-bounce" />
+          <h3 className="mt-2 text-sm font-semibold text-gray-900">
+            Create a new meeting
+          </h3>
+          <p className="mt-1 text-center text-sm text-gray-500">
+            Analyse your meeting with ByteBlaze.
+            <br />
+            Powered by AI.
+          </p>
+          <div className="mt-6">
+            <Button disabled={isUploading}>
+              <Upload className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+              Upload Meeting
+              <input className="hidden" {...getInputProps()} />
+            </Button>
+          </div>
+        </>
+      )}
+      {isUploading && (
+        <div className="">
+          <CircularProgressbar
+            value={progress}
+            text={`${progress}%`}
+            className="size-20"
+            styles={buildStyles({
+              pathColor: "#2563eb",
+              textColor: "#2563eb",
+            })}
+          />
+          <p className="text-center text-sm text-gray-500">
+            Uploading your meeting...
+          </p>
+        </div>
+      )}
     </Card>
   );
 };
